79e6209925b21d136853f61444c9ea133af9a62c
Klavs Kalnins <klavs.kalnins@beetrootlab.com>
2024-05-27 15:58:48 +0300

    finish

---
+++ components/GlitchEffect.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface Props {
+  inputValue: string;
+}
+
+const GlitchEffect: React.FC<Props> = ({ inputValue }) => {
+  // Split the input value into an array of characters
+  const characters: string[] = inputValue.split("");
+
+  while (characters.length < 8) {
+    characters.push(" "); // Push spaces until the length reaches 9
+  }
+  // Create an array with 9 copies of the characters
+  const lines: string[][] = Array(9).fill(characters);
+
+  return (
+    <div className="glitch text-[3rem] xl:text-[80px]">
+      {/* Map over each line */}
+      {lines.map((line, index) => (
+        <div key={index} className="line">
+          {/* Map over each character in the line */}
+          {line.map((char: string, charIndex: number) => (
+            <div
+              key={charIndex}
+              className="char-wrapper inline-block relative mx-1.5 xl:mx-4"
+            >
+              {/* Render the character */}
+              {char == " " ? (
+                <span className="char opacity-0">A</span>
+              ) :
+              <span className="char">{char}</span>
+              }
+              {/* Render the green line */}
+              <div className="absolute bottom-0 left-0 w-full h-1.5 bg-green-500"></div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GlitchEffect;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import GlitchEffect from "../components/GlitchEffect";
 import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,10 +8,15 @@ export default function Home() {
   const [inputValue, setInputValue] = useState("");
   const [isUnlocked, setUnlocked] = useState(false);
   const [showWrongKey, setShowWrongKey] = useState(false);
-  const secret = "key";
+  const secret = process.env.SECRET_KEY;// "ABCRKSPO";
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value.toUpperCase().slice(0, 8));
+  };
+
   const handleClick = () => {
+    console.log(secret);
     if (inputValue.trim() == secret) {
       setUnlocked(true);
       setShowWrongKey(false);
@@ -25,19 +30,20 @@ export default function Home() {
 
   const delayedFunction = () => {
     setShowWrongKey(true);
+    setInputValue("");
     const id = setTimeout(() => {
       setShowWrongKey(false);
-    }, 3000);
+    }, 2000);
     setTimeoutId(id);
   };
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 tracking`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-center flex-col gap-4 font-mono text-sm flex">
+      <div className="w-screen h-screen flex justify-center items-center">
         {isUnlocked ? (
-          <div className="w-full">
+          <div className="w-full flex justify-center">
             <video controls autoPlay width="1920" height="1080">
               <source src="video.mp4" type="video/mp4" />
               Your browser does not support the video tag.
@@ -45,20 +51,30 @@ export default function Home() {
           </div>
         ) : (
           <>
-            <textarea
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              placeholder="insert..."
-              className="w-full h-56 border border-indigo-300 rounded px-3 py-2 mr-2 text-white bg-slate-800"
-              autoComplete="off"
-            />
-            <button
-              onClick={handleClick}
-              className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Try your luck!
-            </button>
-            {showWrongKey && <p className="text-red-500">Wrong guess</p>}
+            <div className="w-full xs:w-full xl:w-1/2 h-1/5 bg-red-500/0 flex flex-col items-center gap-10 z-0">
+              <GlitchEffect inputValue={inputValue} />
+            </div>
+
+            <div className="absolute w-full xs:w-full xl:w-1/2 h-1/5 bg-red-500/0 flex flex-col gap-10">
+              <input
+                className="w-full h-full opacity-0 relative z-10 text-black text-8xl text-center"
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder=""
+              />
+              <div className="bg-green-400/0 w-full h-20 flex justify-center items-center">
+                <button
+                  onClick={handleClick}
+                  className={`${showWrongKey ? 'bg-red-400' : 'bg-green-400'} h-full px-10 hover:brightness-90 text-white font-bold py-2`}
+                >
+                  <p className="text-black">{showWrongKey ? 'WRONG' : 'ENTER'}</p>
+                </button>
+              </div>
+            </div>
+            <div className="absolute bottom-10 w-1/2 sm:w-1/3 xl:w-1/6">
+              <img src="/dystopia_logo.png" />
+            </div>
           </>
         )}
       </div>
